00ed6d65d3049282621a3abb15c5d3d5a6715aef
betwar512 <betwar512@live.com>
2014-09-23 11:02:17 +1000

    added comment to be clear for reader

--- RestaurantUnitTest.Tests/Controllers/UnitTest1.cs
+++ RestaurantUnitTest.Tests/Controllers/UnitTest1.cs
@@ -10,25 +10,21 @@ using RestaurantUnitTest.Controllers;
 
 namespace RestaurantUnitTest.Tests.Controllers
 {
-
+    //class Order created here 3 propery 
     public class order
     {
        public string name { get; set; }
-
-
-       public DateTime date
-
-       { get; set; }
-
+       public DateTime date { get; set; }
+       public int price { get; set; }
+        //string without setter just getter 
        public string familyName
        {
-           get { return "safaie"; }
-        
+           get { return "safaie"; }  
        }
-       public int price { get; set;}
-
- 
+  
     }
+
+    //new object Item 
     public class Item
     {
         public int price { get; set; }
@@ -38,8 +34,10 @@ namespace RestaurantUnitTest.Tests.Controllers
     [TestClass]
     public class UnitTest1
     {
+        //test method just to Initilize object and give it property 
+
         [TestMethod]
-        public void firstTest()
+        public void newObjectTest()
         {
             order myorder = new order();
             myorder.name = "john";
@@ -48,23 +46,37 @@ namespace RestaurantUnitTest.Tests.Controllers
             Console.WriteLine("{0} {1} {2}", myorder.name, myorder.date,st);
             
         }
+
+        //total property to add all prices for Items together 
         public int total;
+
+        /*methos for test tootal from our Array in c# 
+         *method created 3 insted of object Item with seted properties 
+         *create List (Array) and Add each item to Array 
+         *Create foreach loop to het object out and add price to our total property 
+         */
+        
          [TestMethod]
         public void TotalTest()
         {
             List<Item> myList = new List<Item>();
-
+             //item1
             Item item1 = new Item();
             item1.name = "james";
             item1.price = 500;
+             //add to list 
             myList.Add(item1);
-            Item item2 = new Item();
+            //item2
+             Item item2 = new Item();
             item2.name = "jones";
             item2.price = 550;
+            //add to list 
             myList.Add(item2);
-            Item item3 = new Item();
+            //item3
+             Item item3 = new Item();
             item3.name = "Cale";
             item3.price = 700;
+            //add to list 
             myList.Add(item3);
          
             foreach (var i in myList)
