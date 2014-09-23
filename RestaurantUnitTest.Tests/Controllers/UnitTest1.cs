b040f47a1966304f3cbb365bae4a35a0c384f0e9
betwar512 <betwar512@live.com>
2014-09-23 10:42:45 +1000

    new test method added to UnitTest1

--- RestaurantUnitTest.Tests/Controllers/UnitTest1.cs
+++ RestaurantUnitTest.Tests/Controllers/UnitTest1.cs
@@ -10,6 +10,7 @@ using RestaurantUnitTest.Controllers;
 
 namespace RestaurantUnitTest.Tests.Controllers
 {
+
     public class order
     {
        public string name { get; set; }
@@ -24,8 +25,16 @@ namespace RestaurantUnitTest.Tests.Controllers
            get { return "safaie"; }
         
        }
+       public int price { get; set;}
+
  
     }
+    public class Item
+    {
+        public int price { get; set; }
+        public string name { get; set; }
+    }
+   
     [TestClass]
     public class UnitTest1
     {
@@ -36,8 +45,36 @@ namespace RestaurantUnitTest.Tests.Controllers
             myorder.name = "john";
             myorder.date = DateTime.Now;
             var st = myorder.familyName;
-            Console.WriteLine("{0} {1}", myorder.name, myorder.date);
-            Console.ReadLine();
+            Console.WriteLine("{0} {1} {2}", myorder.name, myorder.date,st);
+            
+        }
+        public int total;
+         [TestMethod]
+        public void TotalTest()
+        {
+            List<Item> myList = new List<Item>();
+
+            Item item1 = new Item();
+            item1.name = "james";
+            item1.price = 500;
+            myList.Add(item1);
+            Item item2 = new Item();
+            item2.name = "jones";
+            item2.price = 550;
+            myList.Add(item2);
+            Item item3 = new Item();
+            item3.name = "Cale";
+            item3.price = 700;
+            myList.Add(item3);
+         
+            foreach (var i in myList)
+            {
+                var j = i.price;
+                total += j;
+            }
+
+            Console.WriteLine("{0}",total);
+         
         }
     }
 }
